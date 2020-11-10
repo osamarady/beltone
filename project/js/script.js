4cdbe6a222fa7595b58c6d377c2856acf36a7793
unknown <osamarady1@yahoo.com>
2020-11-10 10:45:40 +0200

    up

--- project/js/script.js
+++ project/js/script.js
@@ -22,3 +22,26 @@ var swiper = new Swiper('.swiper-container', {
 	  prevEl: '.swiper-button-prev',
 	},
 });
+
+function menuOpener() {
+	document.getElementById("megaMenu").classList.add("open");
+}
+function menuCloser() {
+	document.getElementById("megaMenu").classList.remove("open");
+}
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
